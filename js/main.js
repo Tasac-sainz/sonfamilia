49755906612f414dc3b0120f65f7e84bb62a4140
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-08-28 21:17:55 +0200

    buscador de razas de dog-page funcional

--- js/main.js
+++ js/main.js
@@ -9,8 +9,51 @@ burger.addEventListener ("click", () => {
 })
 
 const selectDog = document.querySelector(".select-dog_form");
-const imgDog = document.querySelector(".dog-img")
+const imgDog = document.querySelector(".dog-img");
+const textDescrip = document.querySelector(".dog-description")
 
-const handleClickSelectDog = () => {
 
+const breedDescription = 
+    {"hound/ibizan":"Es el perro más inteligente del mundo", 
+    "husky": "Son independientes y cantarines"};
+
+
+const buildImg = (url) => {
+     fetch ("https://dog.ceo/api/breeds/image/random")          /* Este fetch pinta una imagen aleatoria cada vez que se carga la página */
+    .then ((response) => {
+        return response.json()})
+    .then ((data) => {
+        imgDog.src = data.message
+        })
+    .catch((error) => {
+    console.log("Se ha producido un error:", error);
+    });
 }
+buildImg();
+
+const showSelectDog = (selectBreed) => {
+    fetch (`https://dog.ceo/api/breed/${selectBreed}/images/random`)
+    .then ((response) => {
+        return response.json()})
+    .then ((data) => {
+        imgDog.src = data.message
+        })
+    .catch((error) => {
+    console.log("Se ha producido un error:", error);
+    });
+}
+
+const showDescripDog = (breed) => {
+    textDescrip.classList.remove("quote");
+    const descr = breedDescription[breed];
+    if (descr) {
+        textDescrip.innerHTML = descr
+    }};
+
+const handleClickSelectDog = (event) => {
+    const selectBreed = event.target.value;
+    showSelectDog(selectBreed);
+    showDescripDog (selectBreed);
+}
+
+selectDog.addEventListener("change", handleClickSelectDog);
