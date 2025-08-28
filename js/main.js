4527ad140c3521b3d6420bed897cf5efb2dd5ddd
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-08-28 13:01:33 +0200

    iniciando dog-page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 'use strict'
 console.log("ready!");
 
-const header = document.querySelector(".header");
-const footer = document.querySelector(".footer");
+const burger = document.querySelector(".burger-menu");
+const menuList = document.querySelector(".list");
 
-fetch ("/partials/_header.html").then (response => response.text()).then (html => {
-    header.innerHTML=html
-    const burger = document.querySelector(".burger-menu");
-    const menuList = document.querySelector(".list");
-    burger.addEventListener ("click", () => {
-        menuList.classList.toggle("hidden");
-    })
-});
-
-fetch ("/partials/_footer.html").then (response => response.text()).then (html => {footer.innerHTML=html});
+burger.addEventListener ("click", () => {
+    menuList.classList.toggle("hidden");
+})
