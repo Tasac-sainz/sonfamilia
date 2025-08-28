ab56e1432b143a521fc5746727e7cb820620c05a
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-08-28 11:03:04 +0200

    Home terminada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 'use strict'
 console.log("ready!");
 
-const burger = document.querySelector(".burger-menu");
-const menuList = document.querySelector(".list");
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
 
-burger.addEventListener ("click", () => {
-    menuList.classList.toggle("hidden");
-})
+fetch ("/partials/_header.html").then (response => response.text()).then (html => {
+    header.innerHTML=html
+    const burger = document.querySelector(".burger-menu");
+    const menuList = document.querySelector(".list");
+    burger.addEventListener ("click", () => {
+        menuList.classList.toggle("hidden");
+    })
+});
+
+fetch ("/partials/_footer.html").then (response => response.text()).then (html => {footer.innerHTML=html});
