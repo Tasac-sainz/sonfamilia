bb16e4ba54f1812c2b0dc0662d6abaeebe88d675
Tania S. Sainz <tssainzdeaja@gmail.com>
2025-08-29 13:31:54 +0200

    contact-page en la última fase de edición

--- js/vet.js
+++ js/vet.js
@@ -7,16 +7,18 @@ const vetCardContainer = document.querySelector(".vets");
 
 
 const vets = [
-    {name:"Home Vets", url: "https://media.v2.siweb.es/uploaded_thumb_small/b53d701788f2aec074f342d04ff697a2/logo_2.png" ,descrip:"Destaca por su honradez, grandes profesionales con servicio integral veterinario", direction: "Cotos de Monterey"},
-    {name: "Las 4 Huellas", url:"https://www.las4huellas.es/data/6457/modulos/2555207/las_4_huellas.png", descrip: "Buen trato y empatía hacia los animales y referentes, especilidades poco comunes como la Terapia Hormonal Sustitutiva", direction: "San Agustín del Guadalix"},
-    {name: "Sirio", url:"https://lh5.googleusercontent.com/proxy/rAG81O67GQoSlAXuMjKPGselzwVOE2Dk5tx8KJqNvb0vh7WbIGUwGXzdjAzkZ3JQMQrVq8rNhB5SAOT1Z-nybcEckFRpgnSQS4s9QZG-hdVtgwauFocmouGc", descrip: "Amabilidad y trato respetuoso, personal encantador con servicio de peluquería", direction: "Pedrezuela"}
+    {name:"Home Vets", url: "https://media.v2.siweb.es/uploaded_thumb_small/b53d701788f2aec074f342d04ff697a2/logo_2.png" ,descrip:"Destaca por su honradez, grandes profesionales con servicio integral veterinario", direction: "Cotos de Monterey", link: "https://homevets.es/"},
+    {name: "Las 4 Huellas", url:"https://www.las4huellas.es/data/6457/modulos/2555207/las_4_huellas.png", descrip: "Buen trato y empatía hacia los animales y referentes, especilidades poco comunes como la Terapia Hormonal Sustitutiva", direction: "San Agustín del Guadalix", link:"https://www.las4huellas.es/"},
+    {name: "Sirio", url:"https://lh5.googleusercontent.com/proxy/rAG81O67GQoSlAXuMjKPGselzwVOE2Dk5tx8KJqNvb0vh7WbIGUwGXzdjAzkZ3JQMQrVq8rNhB5SAOT1Z-nybcEckFRpgnSQS4s9QZG-hdVtgwauFocmouGc", descrip: "Amabilidad y trato respetuoso, personal encantador con servicio de peluquería", link:"http://www.cvsirio.es/", direction: "Pedrezuela"}
 ]
 
 vets.forEach ((vet) => {
     const cardVet =
          `<div class="vets-card">
             <h3 class="vets-card_title">${vet.name}</h3>
-            <img class="vets-card_img" src="${vet.url}"/>
+            <a href="${vet.link}" target="_blank">
+                <img class="vets-card_img" src="${vet.url}"/>
+            </a>
              <p class="vets-card_descrip">${vet.descrip}</p>
             <p class="vets-card_direction">${vet.direction}</p>
         </div>`
